4a97107b1f43adc1f4e103eeb1bbe8780c6a08a5
AzhanTechWhiz <141222013+AzhanTechWhiz@users.noreply.github.com>
2024-01-06 19:32:38 +0500

    Fix Designs (#3)
    * feat: install new packages
    
    * feat: setup app & routes with forms
    
    * feat: update placeholders
    
    * fix: typo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar.js';
 import Hero from './components/Hero.js';
+import Stocks from './components/Stocks.js';
 
 function App() {
   return (
-    <div>
-     <Navbar/>
-     <Hero/>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Hero />} />
+        <Route exact path="/stocks" element={<Stocks />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,31 @@
-import React from 'react'
-
-import logo from './tani.png'
-
-import './components.css'
+import React from 'react';
+import logo from './tani.png';
+import './components.css';
+import { Link } from "react-router-dom"
 
 export default function Navbar() {
-    return (
-        <>
-
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="container-fluid">
-                    {/* Navbar on the left */}
-                    <div className="d-flex align-items-center">
-                        <a className="navbar-brand" href="/"><img src={logo} alt="" /></a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                    </div>
-                    {/* List on the right */}
-                    <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-                        <ul className="navbar-nav mx-3">
-                            <li className="nav-item mx-3">
-                                <a className="nav-link" href="/">Home</a>
-                            </li>
-                            <li className="nav-item mx-3">
-                                <a className="nav-link" href="/">About</a>
-                            </li>
-                            <li className="nav-item mx-3">
-                                <a className="nav-link" href="/">Blog</a>
-                            </li>
-                            <li className="nav-item mx-2">
-                                <a className="nav-link" href="/">Contact</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </>
-    )
+  return (
+    <nav className="navbar navbar-expand-lg shadow-lg">
+      <div className="container-fluid">
+        <div className="d-flex align-items-center">
+          <a className="navbar-brand" href="/"><img src={logo} alt="" /></a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </div>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+          <ul className="navbar-nav mx-3"> 
+            <li className="nav-item">
+              <Link exact to="/" className="nav-link mx-3" activeClassName="active-link">
+                Home
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link mx-3" to="/stocks">Stock-Adjustment</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
