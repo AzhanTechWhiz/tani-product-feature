1000fb08daa33fecd263bff3f27b2efb151777b2
azhan malik <azhanmalik046@gmail.com>
2024-01-08 16:52:12 +0500

    add input fields

---
+++ src/components/Manufacturing.js
@@ -0,0 +1,80 @@
+import React from "react";
+
+export default function Manufacturing() {
+  return (
+    <>
+      <div style={{ padding: "40px" }}>
+        <h1>Manufacture Items :</h1>
+        <hr className="line" />
+        <div className="">
+          <div className="">
+            <div className="row align-items-center ">
+              <div className="col-2">
+              <h4 className="fw-bold my-4">8 + 2</h4>
+              </div>
+              <div className="col-4">
+              <input
+                type="text"
+                name="Manufacture Items"
+                className="form-control mb-2"
+                placeholder="Enter Here :"
+              />
+              </div>
+              <div className="col-2">
+              <h4 className="fw-bold my-4">Pieces</h4>
+              </div>
+            </div>
+            <div className="row align-items-center ">
+              <div className="col-2">
+              <h4 className="fw-bold my-4">6 + 2</h4>
+              </div>
+              <div className="col-4">
+              <input
+                type="text"
+                name="Manufacture Items"
+                className="form-control mb-2"
+                placeholder="Enter Here :"
+              />
+              </div>
+              <div className="col-2">
+              <h4 className="fw-bold my-4">Pieces</h4>
+              </div>
+            </div>
+            <div className="row align-items-center ">
+              <div className="col-2">
+              <h4 className="fw-bold my-4">Switches</h4>
+              </div>
+              <div className="col-4">
+              <input
+                type="text"
+                name="Manufacture Items"
+                className="form-control mb-2"
+                placeholder="Enter Here :"
+              />
+              </div>
+              <div className="col-2">
+              <h4 className="fw-bold my-4">Pieces</h4>
+              </div>
+            </div>
+            <div className="row align-items-center ">
+              <div className="col-2">
+              <h4 className="fw-bold my-4">LED Bulb</h4>
+              </div>
+              <div className="col-4">
+              <input
+                type="text"
+                name="Manufacture Items"
+                className="form-control mb-2"
+                placeholder="Enter Here :"
+              />
+              </div>
+              <div className="col-2">
+              <h4 className="fw-bold my-4">Pieces</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
